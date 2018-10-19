7081cfaa55c8e4d79b84297631fba70e0ccb15f3
Te Cai <tsai_te@Tes-MacBook-Air.local>
2018-10-19 16:39:39 -0400

    api updates

--- src/main/java/com/te/config/MvcConfig.java
+++ src/main/java/com/te/config/MvcConfig.java
@@ -1,11 +1,55 @@
 package com.te.config;
 
+import com.te.config.viewresolver.JsonViewResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.ResourceHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.te.api")
-public class MvcConfig {
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Bean
+    public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager){
+        ContentNegotiatingViewResolver resolver=new ContentNegotiatingViewResolver();
+        resolver.setContentNegotiationManager(manager);
+
+        List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+        resolvers.add(jsonViewResolver());
+        //resolver.add(xmlViewResolver());
+
+        resolver.setViewResolvers(resolvers);
+        return resolver;
+    }
+
+    public ViewResolver jsonViewResolver(){
+        return new JsonViewResolver();
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters){
+        MappingJackson2HttpMessageConverter jsonViewHttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        ResourceHttpMessageConverter resourceHttpMessageConverter = new ResourceHttpMessageConverter();
+        converters.add(jsonViewHttpMessageConverter);
+        converters.add(resourceHttpMessageConverter);
+    }
 }
