dfceaa62108eb516d15a8d63ddcdc2082c422b1b
Te Cai <tsai_te@Tes-MacBook-Air.fios-router.home>
2018-10-18 23:34:50 -0400

    controller updates

--- src/main/java/com/te/service/CulturalInheritanceService.java
+++ src/main/java/com/te/service/CulturalInheritanceService.java
@@ -1,9 +1,12 @@
 package com.te.service;
 
 import com.te.domain.CulturalInheritance;
+import com.te.domain.User;
 import com.te.repository.CulturalInheritanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CulturalInheritanceService {
@@ -12,6 +15,17 @@ public class CulturalInheritanceService {
 
     public CulturalInheritance findByCategory(String name){
         CulturalInheritance result=culturalInheritanceRepository.findByCategoryIgnoreCase(name);
+        return result;
+    }
+
+    public CulturalInheritance findById(Long Id){
+        Optional<CulturalInheritance> result=culturalInheritanceRepository.findById(Id);
+        CulturalInheritance culturalInheritance = result.get();
+        return culturalInheritance;
+    }
+
+    public CulturalInheritance createCulturalInheritance(CulturalInheritance culturalInheritance){
+        CulturalInheritance result=culturalInheritanceRepository.save(culturalInheritance);
         return result;
     }
 }
