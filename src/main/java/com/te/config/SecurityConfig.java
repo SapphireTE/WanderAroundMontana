3901a977dc8d20987708f4d7773c0b02eb44367d
Te Cai <tsai_te@Tes-MacBook-Air.local>
2018-10-26 17:14:15 -0400

    userdetailsserviceimpl updates

--- src/main/java/com/te/config/SecurityConfig.java
+++ src/main/java/com/te/config/SecurityConfig.java
@@ -5,14 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+//@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-            @Autowired
-            private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+public class SecurityConfig {
+//            @Autowired
+//            private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
 //    step1
 //    @Autowired
 //    public void configureGlobal(AuthenticationManagerBuilder auth)
@@ -42,5 +47,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin();
 //                .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint);
+    }
+    @Configuration
+    public static class RestWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        @Autowired
+        private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+        //curl -i -X POST -d username=user -d password=password -c ./cookies.txt http://localhost:8080/login
+        @Autowired
+        private UserDetailsService userDetailsService;
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//            PasswordEncoder encoder = new BCryptPasswordEncoder();
+            auth.inMemoryAuthentication().withUser("user")
+                .password("{noop}password").roles("REGISTERED_USER");
+//            auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+        }
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring()
+                    .antMatchers("/resources/**");
+        }
+        protected void configure(HttpSecurity http) throws Exception {
+            //http://www.baeldung.com/securing-a-restful-web-service-with-spring-security
+            http.csrf().disable().authorizeRequests().antMatchers("/api/users/login","/api/users/signup").permitAll()
+                    .and()
+                    .authorizeRequests().antMatchers("/api/**").authenticated()
+                    .and()
+                    .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint)
+                    .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .formLogin();
+        }
     }
 }
--- src/main/java/com/te/domain/User.java
+++ src/main/java/com/te/domain/User.java
@@ -1,13 +1,17 @@
 package com.te.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.time.Instant;
+import java.util.Collection;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
 @Entity
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = SEQUENCE, generator ="users_id_seq")
@@ -49,8 +53,34 @@ public class User {
     public String getUsername(){
         return this.username;
     }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
     public void setUsername(String username){
         this.username=username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 
     public String getPassword(){
