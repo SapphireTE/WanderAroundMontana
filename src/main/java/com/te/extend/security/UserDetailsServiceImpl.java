5f8e4586374de9fbdfb1db333328ef2221175490
Te Cai <tsai_te@Tes-MacBook-Air.local>
2018-10-26 17:14:15 -0400

    userdetailsserviceimpl updates

---
+++ src/main/java/com/te/extend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.te.extend.security;
+
+import com.te.domain.User;
+import com.te.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSourceAware;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username){
+        User result=userService.findByUsernameIgnoreCase(username);
+        return result;
+    }
+
+
+}
