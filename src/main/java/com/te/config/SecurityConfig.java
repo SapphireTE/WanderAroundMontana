507893109421406b23828318d32629be72f95c1e
Te Cai <tsai_te@Tes-MacBook-Air.local>
2018-11-02 17:40:26 -0400

    authentication manager

--- src/main/java/com/te/config/SecurityConfig.java
+++ src/main/java/com/te/config/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.te.config;
 
 import com.te.extend.security.RestAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -67,6 +70,13 @@ public class SecurityConfig {
             web.ignoring()
                     .antMatchers("/resources/**");
         }
+
+        @Bean(name= BeanIds.AUTHENTICATION_MANAGER)
+        @Override
+        public AuthenticationManager authenticationManagerBean() throws Exception{
+            return super.authenticationManagerBean();
+        }
+
         protected void configure(HttpSecurity http) throws Exception {
             //http://www.baeldung.com/securing-a-restful-web-service-with-spring-security
             http.csrf().disable().authorizeRequests().antMatchers("/api/users/login","/api/users/signup").permitAll()
