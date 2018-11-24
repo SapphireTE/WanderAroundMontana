e32387cbc43b3be949a98d2f079935d5f9aed31f
Te Cai <tsai_te@Tes-MacBook-Air.local>
2018-11-24 14:58:51 -0500

    storage service updates

---
+++ src/main/java/com/te/service/StorageService.java
@@ -0,0 +1,32 @@
+package com.te.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+
+import java.io.File;
+
+public class StorageService {
+    private AmazonS3 s3;
+    private String bucket;
+
+    public StorageService(AmazonS3 s3){
+        this.s3=s3;
+    }
+
+    public StorageService(){}
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
+    public void putObject(String S3key, File file){
+        s3.putObject(bucket,S3key,file);
+    }
+
+    public void putObject(String bucket, String S3key, File file){
+        s3.putObject(bucket, S3key, file);
+    }
+}
---
+++ src/test/java/com/te/service/StorageServiceTest.java
@@ -0,0 +1,41 @@
+package com.te.service;
+
+import com.amazonaws.services.cloudsearchdomain.model.Bucket;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.te.config.AppConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.io.File;
+
+import static junit.framework.TestCase.assertTrue;
+
+@WebAppConfiguration
+@ContextConfiguration(classes = {AppConfig.class})
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("unit")
+public class StorageServiceTest {
+
+//    @Autowired
+//    private StorageService storageService;
+
+    @Test
+    public void putObjectTest (){
+        AmazonS3 s3=AmazonS3ClientBuilder.standard().withRegion("us-east-1").build();
+        //AmazonS3 s3=AmazonS3ClientBuilder.defaultClient();
+        StorageService storageService=new StorageService(s3);
+        String s3key="tt";
+        File file=new File("/Users/tsai_te/Desktop/Screen Shot 2018-10-27 at 11.48.30 AM.png");
+        String bucket="mt.project";
+        storageService.putObject(bucket, s3key,file);
+        assertTrue(false);
+
+    }
+
+}
