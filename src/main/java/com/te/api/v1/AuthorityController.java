2eab8f6bf4378fca66a23c8c2531f9fcda644d41
Te Cai <tsai_te@Tes-MacBook-Air.local>
2018-12-21 17:13:48 -0500

    controller and JsView updates

--- src/main/java/com/te/api/v1/AuthorityController.java
+++ src/main/java/com/te/api/v1/AuthorityController.java
@@ -1,28 +1,28 @@
-package com.te.api.v1;
-
-import com.te.domain.Authority;
-import com.te.domain.User;
-import com.te.service.AuthorityService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping(value="/api/authorities")
-public class AuthorityController {
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
-    @Autowired
-    private AuthorityService authorityService;
-
-
-//    @RequestMapping(value = "",method = RequestMethod.GET)
-//    public Authority findAuthoritiesByUser(@PathVariable("Authority") List<Authority> Authority) {
-//        logger.debug("parameter name is:" +Authority);
-//        Authority result = authorityService.findAuthoritiesByUser(Authority);
-//        return result;
-//    }
-}
+//package com.te.api.v1;
+//
+//import com.te.domain.Authority;
+//import com.te.domain.User;
+//import com.te.service.AuthorityService;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping(value="/api/authorities")
+//public class AuthorityController {
+//    private final Logger logger = LoggerFactory.getLogger(getClass());
+//
+//    @Autowired
+//    private AuthorityService authorityService;
+//
+//
+////    @RequestMapping(value = "",method = RequestMethod.GET)
+////    public Authority findAuthoritiesByUser(@PathVariable("Authority") List<Authority> Authority) {
+////        logger.debug("parameter name is:" +Authority);
+////        Authority result = authorityService.findAuthoritiesByUser(Authority);
+////        return result;
+////    }
+//}
