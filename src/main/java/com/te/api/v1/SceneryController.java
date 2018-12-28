ad21b2122429d5e9040e24678d17fc28cd8ef05b
Te Cai <tsai_te@Tes-MacBook-Air.local>
2018-12-28 14:21:37 -0500

    re-migrating the database and altering image table

--- src/main/java/com/te/api/v1/SceneryController.java
+++ src/main/java/com/te/api/v1/SceneryController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value="/api/scereries")
+@RequestMapping(value={"/api/scereries"})
 public class SceneryController {
     private final Logger logger= LoggerFactory.getLogger(getClass());
 
