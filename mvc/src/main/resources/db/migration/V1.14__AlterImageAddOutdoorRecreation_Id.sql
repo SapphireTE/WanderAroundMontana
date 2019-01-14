21fb952aea8f49158fa64d0689f17157889d1a61
Te Cai <tsai_te@Tes-MacBook-Air.local>
2019-01-14 15:23:39 -0500

    add outdoorrecreation id

--- mvc/src/main/resources/db/migration/V1.14__AlterImageAddOutdoorRecreation_Id.sql
+++ mvc/src/main/resources/db/migration/V1.14__AlterImageAddOutdoorRecreation_Id.sql
@@ -1,6 +1,7 @@
--- outdoorRecreation_id default null,
--- constraint fk_Image_Outdoor_Recreation
---     foreign key (outdoorRecreation_id)
---     references Outdoor_Recreations (id)
---     on delete no action
---     on update no action;
+alter table Images add column outdoorRecreation_id bigint default null;
+
+alter table Images add constraint fk_Image_Outdoor_Recreation
+    foreign key (outdoorRecreation_id)
+    references Outdoor_Recreations (id)
+    on delete no action
+    on update no action;
