7e917cd7b414649b2eb8901670ffd4110e5bd0be
Te Cai <tsai_te@Tes-MacBook-Air.fios-router.home>
2019-02-24 21:58:57 -0500

    image repository, service, and controller updates

--- mvc/src/main/java/com/te/repository/ImageRepository.java
+++ mvc/src/main/java/com/te/repository/ImageRepository.java
@@ -2,15 +2,30 @@ package com.te.repository;
 
 import com.te.domain.Image;
 import com.te.domain.Scenery;
+import com.te.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface ImageRepository extends CrudRepository<Image,Long> {
     Image findByImageName(String imageName);
 //    Image uploadImage ()
-    Image findByUploadDate(LocalDate uploadDate);
+//    General
+    List<Image> findByUploadDate(LocalDate uploadDate);
     Image findByUuid (String uuid);
-    Image findByScenery (Scenery scenery);
+
+//    User
+    Image findByUser(User user);
+
+//    Scenery
+    List<Image> findByScenery (Scenery scenery);
+    List<Image> findBySceneryCategory (String category);
+
+//todo
+
+//    CulturalInheritance
+
+//    OutdoorRecreation
 
 }
